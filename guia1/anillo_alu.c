ea8f1e819724441e8b21a7da66ce68f01c0f50ce
tobiasperel <peretobias@gmail.com>
2025-04-14 15:36:19 -0300

    ej2 limpio

--- guia1/anillo_alu.c
+++ guia1/anillo_alu.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <time.h>
+#include <stdbool.h>
 // #include "constants.h"
 #include <limits.h>
 
@@ -18,10 +19,8 @@ int wrapfwd(int i, int n) {
 void close_ring_pipes_for_process(int id, int pipe_table[][2], int table_size) {
 	for (int i = 0; i < table_size; i++) {
 		if (i == id) {
-			// Close only write end fd
 			close(pipe_table[i][1]);
 		} else if (i == wrapfwd(id, table_size)) {
-			// Close only read end fd
 			close(pipe_table[i][0]);
 		} else {
 			close(pipe_table[i][0]);
@@ -47,87 +46,60 @@ int main(int argc, char **argv)
   	/* COMPLETAR */
     printf("Se crearán %i procesos, se enviará el caracter %i desde proceso %i \n", n, buffer, start);
 
-	// Create all n pipes of the process ring
 	int pipe_fds[n][2];
 	for (int i = 0; i < n; i++) {
-		if (pipe(pipe_fds[i]) == -1) {
-			printf("Pipe %d not created correctly\n", i);
-		}
+		pipe(pipe_fds[i]);
+		
 	}
 
-	// Create both father <-> start process pipes
 	int pipe_to_start[2];
 	int pipe_from_start[2];
-	if (pipe(pipe_to_start) == -1) {
-		printf("Pipe to start not created correctly\n");
-	}
-	if (pipe(pipe_from_start) == -1) {
-		printf("Pipe from start not created correctly\n");
-	}
+	pipe(pipe_to_start);
+	pipe(pipe_from_start);
 
-	// Create all the n ring child processes with fork()
 	pid_t child_pid;
 	int prev_pipe[2];
 	int next_pipe[2];
-	int is_start_process = 0;
+	bool is_start_process = false;
 	for (int i = 0; i < n; i++) {
 		child_pid = fork();
-		if (child_pid < 0) {
-			printf("Fork of child %d failed\n", i);
-		} else if (child_pid == 0) {
-			// Execute only in child process number i
+		if (child_pid == 0) {
 
-			// Close all unused ring pipes, copy relevant pipes to variables
 			prev_pipe[0] = pipe_fds[i][0];
 			prev_pipe[1] = pipe_fds[i][1];
 			next_pipe[0] = pipe_fds[(wrapfwd(i,n))][0];
 			next_pipe[1] = pipe_fds[(wrapfwd(i,n))][1];
 			close_ring_pipes_for_process(i, pipe_fds, n);
 
-			// Close unused father <-> start pipes, if necessary
 			if (i == start) {
-				// Execute this code only for the start ring process
 				is_start_process = 1;
 				close(pipe_to_start[1]);
 				close(pipe_from_start[0]);
 			} else {
-				// Execute this code only for non-start ring processes
 				close(pipe_to_start[0]);
 				close(pipe_to_start[1]);
 				close(pipe_from_start[0]);
 				close(pipe_from_start[1]);
 			}
-
-			// Now all unused pipes should be closed regardless of child process number
-			// Now break out of for loop and execute child process code within conditional below
 			break;
 		}
 	}
 
 	if (child_pid != 0) {
-		// Only execute this code for father process
-		// Close unused pipes for father process
 		close(pipe_to_start[0]);
 		close(pipe_from_start[1]);
 		for (int i = 0; i < n; i++) {
 			close(pipe_fds[i][0]);
 			close(pipe_fds[i][1]);
 		}
-	}
 
-	if (child_pid != 0) {
-		// Execute main functionality for father process
-		// All unused pipe fds are closed
-
-		// I assume that the initial value of the message is the variable named 'buffer'
 		int initial_message = buffer;
 		write(pipe_to_start[1], &initial_message, sizeof(initial_message));
 		close(pipe_to_start[1]);
 		int final_value;
 		read(pipe_from_start[0], &final_value, sizeof(final_value));
 		close(pipe_from_start[0]);
-		// Now final_value must be outputted to stdout
-		printf("Communication finished with final value: %d", final_value);
+		printf("Communication finished with final value: %d \n", final_value);
 		for (int i = 0; i < n; i++) {
 			wait(NULL);
 		}
@@ -135,18 +107,13 @@ int main(int argc, char **argv)
 	}
 
 	if (child_pid == 0 && is_start_process) {
-		// Execute main functionality for start child processes
-		// All unused pipe fds are closed
 
-		// Read initial value provided by father process
 		int initial_value;
 		read(pipe_to_start[0], &initial_value, sizeof(initial_value));
 		close(pipe_to_start[0]);
 
-		// Generate random secret number that is larger than initial value
 		int secret_number = initial_value + 1 + generate_random_number();
 
-		// Read from and write to ring in a loop until a number larger or equal to secret_number is read
 		int message = initial_value;
 		write(next_pipe[1], &message, sizeof(message));
 		while (message < secret_number) {
@@ -157,20 +124,15 @@ int main(int argc, char **argv)
 			write(next_pipe[1], &message, sizeof(message));
 		}
 
-		// Initial process has received message number that is bigger or equal to secret number
-		// Now it must tell following ring process to close write fd, close its fd, and communicate with father process
-		// Close ring pipes, send EOF
 		close(prev_pipe[0]);
 		close(next_pipe[1]);
-		// Communicate with father
+
 		write(pipe_from_start[1], &message, sizeof(message));
 		close(pipe_from_start[1]);
 		exit(EXIT_SUCCESS);
 	}
 
 	if (child_pid == 0 && !is_start_process) {
-		// Execute main functionality for non-start child process
-		// All unused pipe fds are closed
 
 		int message = 0;
 		while (1) {
@@ -184,7 +146,4 @@ int main(int argc, char **argv)
 		}
 	exit(EXIT_SUCCESS);
 	}
-
-    /* COMPLETAR */
-    
 }
