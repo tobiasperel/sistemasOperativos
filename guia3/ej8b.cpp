74f5ec59bd2ca4c8e38921fb9407b299e7c61f89
tobiLaptop <pereltobias@gmail.com>
2025-04-23 23:30:29 -0300

    ejercicios

---
+++ guia3/ej8b.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <semaphore> // Requiere C++20
+#include <chrono>
+
+using namespace std;
+
+const int N = 3;
+const int i = 1;
+bool segundaVez = true; 
+
+counting_semaphore<1> sems[N] = {counting_semaphore<1>(0), counting_semaphore<1>(0),
+                                 counting_semaphore<1>(0)};
+
+void proceso(int id)
+{
+    while (1)
+    {
+        sems[id].acquire(); // Wait
+
+        cout << "Proceso " << id << " ejecutando" << endl;
+
+        if (id == 1 && segundaVez)
+        {
+            cout << "Proceso " << id << endl;
+            segundaVez = false;
+            sems[1].release();
+        }
+        else
+        {
+            segundaVez = true;
+            int siguiente = (id + 1) % N;
+            sems[siguiente].release();
+        }
+    }
+}
+
+int main()
+{
+    vector<thread> threads;
+
+    for (int id = 0; id < N; ++id)
+    {
+        threads.emplace_back(proceso, id);
+    }
+
+    sems[i].release(); // Despertar al primer hilo (Pi)
+
+    // Esperar a que terminen
+    for (auto &t : threads)
+    {
+        t.join();
+    }
+
+    return 0;
+}
