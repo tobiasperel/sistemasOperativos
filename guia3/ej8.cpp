24994301ba3d502a57737fc41949b025c3595a04
tobiLaptop <pereltobias@gmail.com>
2025-04-25 01:05:07 -0300

    listo calipso

--- guia3/ej8.cpp
+++ guia3/ej8.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <thread>
 #include <vector>
-#include <semaphore> // Requiere C++20
+#include <semaphore> // C++20
 #include <chrono>
 
 using namespace std;
@@ -9,36 +9,30 @@ using namespace std;
 const int N = 3;
 const int i = 0;
 
-counting_semaphore<1> sems[N] = {counting_semaphore<1>(0), counting_semaphore<1>(0),
-                                 counting_semaphore<1>(0)};
+counting_semaphore<1> sems[N] = {
+    counting_semaphore<1>(0),
+    counting_semaphore<1>(0),
+    counting_semaphore<1>(0)
+};
 
-void proceso(int id)
-{
-    while (1)
-    {
-        sems[id].acquire(); // Wait
-
-        cout << "Proceso " << id << " ejecutando" << endl;
-
-        int siguiente = (id + 1) % N;
-        sems[siguiente].release();
-    }
+void proceso(int id) {
+    sems[id].acquire(); // Esperar su turno
+    cout << "Proceso " << id << " ejecutando" << endl;
+    int siguiente = (id + 1) % N;
+    sems[siguiente].release(); // Darle paso al siguiente
 }
 
-int main()
-{
+int main() {
+    sems[i].release(); // Primer hilo
     vector<thread> threads;
 
-    for (int id = 0; id < N; ++id)
-    {
-        threads.emplace_back(proceso, id);
+    while(true) {    
+        for (int id = 0; id < N; ++id) {
+            threads.emplace_back(proceso, id);
+        }
+        
     }
-
-    sems[i].release(); // Despertar al primer hilo (Pi)
-
-    // Esperar a que terminen
-    for (auto &t : threads)
-    {
+    for (auto& t : threads) {
         t.join();
     }
 
--- guia3/ej8b.cpp
+++ guia3/ej8b.cpp
@@ -15,43 +15,38 @@ counting_semaphore<1> sems[N] = {counting_semaphore<1>(0), counting_semaphore<1>
 
 void proceso(int id)
 {
-    while (1)
+
+    sems[id].acquire(); // Wait
+
+    cout << "Proceso " << id << " ejecutando" << endl;
+
+    if (id == 1 && segundaVez)
     {
-        sems[id].acquire(); // Wait
-
-        cout << "Proceso " << id << " ejecutando" << endl;
-
-        if (id == 1 && segundaVez)
-        {
-            cout << "Proceso " << id << endl;
-            segundaVez = false;
-            sems[1].release();
-        }
-        else
-        {
-            segundaVez = true;
-            int siguiente = (id + 1) % N;
-            sems[siguiente].release();
-        }
+        segundaVez = false;
+        sems[1].release();
     }
+    else
+    {
+        segundaVez = true;
+        int siguiente = (id + 1) % N;
+        sems[siguiente].release();
+    }
+
 }
 
 int main()
 {
+    sems[i].release(); // Primer hilo
     vector<thread> threads;
 
-    for (int id = 0; id < N; ++id)
-    {
-        threads.emplace_back(proceso, id);
+    while(true) {    
+        for (int id = 0; id < N; ++id) {
+            threads.emplace_back(proceso, id);
+        }
+        
     }
-
-    sems[i].release(); // Despertar al primer hilo (Pi)
-
-    // Esperar a que terminen
-    for (auto &t : threads)
-    {
+    for (auto& t : threads) {
         t.join();
     }
-
     return 0;
 }
